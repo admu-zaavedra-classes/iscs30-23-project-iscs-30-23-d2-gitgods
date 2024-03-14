64b9b5ee0a9a8340448f0c4a45486e2fb43ac067
magdamitzen <160488831+magdamitzen@users.noreply.github.com>
2024-03-14 16:34:25 +0800

    Update manage.py

--- MyInventorySystem/manage.py
+++ MyInventorySystem/manage.py
@@ -17,7 +17,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    sys.argv.append('8080')
+    if 'runserver' in sys.argv:
+        sys.argv.append(f'0.0.0.0:{PORT}')
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
