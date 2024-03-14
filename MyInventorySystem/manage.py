dbed972246921eea58f5f238af28035bf85c8f33
magdamitzen <160488831+magdamitzen@users.noreply.github.com>
2024-03-14 13:14:07 +0800

    Update manage.py

--- MyInventorySystem/manage.py
+++ MyInventorySystem/manage.py
@@ -7,6 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyInventorySystem.settings')
+    os.environ["RUN_MAIN"] = "true"  # Override default port to 8080
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
